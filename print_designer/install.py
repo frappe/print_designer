9b4069fb09b77fba741bed7c0bdb147254d8e514
Maharshi Patel <maharshi.patel988@gmail.com>
2023-08-05 23:04:41 +0530

    fix: check framework version before install
    Print Designer is not supported in version-14 so only allow installing if framework version is develop or more then v14.

--- print_designer/install.py
+++ print_designer/install.py
@@ -1,8 +1,27 @@
+import click
+import frappe
 from frappe.custom.doctype.custom_field.custom_field import \
     create_custom_fields
 
 from print_designer.custom_fields import CUSTOM_FIELDS
 
+def check_frappe_version():
+	def major_version(v: str) -> str:
+		return v.split(".")[0]
+
+	frappe_version = major_version(frappe.__version__)
+	if int(frappe_version) >= 15:
+	    return
+
+	click.secho(
+		f"You're attempting to install Print Designer with Frappe version {frappe_version}. "
+		"This is not supported and will result in broken install. Please install it using Version 15 or Develop branch.",
+		fg="red",
+	)
+	raise SystemExit(1)
+
+def before_install():
+	check_frappe_version()
 
 def after_install():
 	create_custom_fields(CUSTOM_FIELDS, ignore_validate=True)
