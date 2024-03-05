ca9326555ebc7b633f38663ac61445793b52eafe
Maharshi Patel <maharshi.patel988@gmail.com>
2024-03-05 12:52:16 +0530

    chore: reference original print function
    instead of copying the code, reference the original print util function

--- print_designer/print_designer/client_scripts/point_of_sale.js
+++ print_designer/print_designer/client_scripts/point_of_sale.js
@@ -1,9 +1,9 @@
 // overrides the print util function that is used in the point of sale page.
 // we should ideally change util function in framework to extend it. this is workaround until that.
+const original_util = frappe.utils.print;
 frappe.utils.print = (doctype, docname, print_format, letterhead, lang_code) => {
-	let w;
 	if (frappe.model.get_value("Print Format", print_format, "print_designer")) {
-		w = window.open(
+		let w = window.open(
 			frappe.urllib.get_full_url(
 				"/app/print/" +
 					encodeURIComponent(doctype) +
@@ -16,27 +16,11 @@ frappe.utils.print = (doctype, docname, print_format, letterhead, lang_code) => 
 					(lang_code ? "&_lang=" + lang_code : "")
 			)
 		);
+		if (!w) {
+			frappe.msgprint(__("Please enable pop-ups"));
+			return;
+		}
 	} else {
-		w = window.open(
-			frappe.urllib.get_full_url(
-				"/printview?doctype=" +
-					encodeURIComponent(doctype) +
-					"&name=" +
-					encodeURIComponent(docname) +
-					"&trigger_print=1" +
-					"&format=" +
-					encodeURIComponent(print_format) +
-					"&no_letterhead=" +
-					(letterhead ? "0" : "1") +
-					"&letterhead=" +
-					encodeURIComponent(letterhead) +
-					(lang_code ? "&_lang=" + lang_code : "")
-			)
-		);
-	}
-
-	if (!w) {
-		frappe.msgprint(__("Please enable pop-ups"));
-		return;
+		original_util(doctype, docname, print_format, letterhead, lang_code);
 	}
 };
