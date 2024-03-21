0f7ccfe75c3cfc53af47cf09566bd3b795bc0649
Maharshi Patel <maharshi.patel988@gmail.com>
2024-03-21 17:22:40 +0530

    feat: add isDynamicHeight property for user control
    This property is how user can configure if some element should have fixed height or should be dynamically adjusted.
    This will not ensure that it will have dynamically calculated height as it will depend on several factors
    This is just to give them control to choose between static and dynamic height.

--- print_designer/public/js/print_designer/PropertiesPanelState.js
+++ print_designer/public/js/print_designer/PropertiesPanelState.js
@@ -433,6 +433,43 @@ export const createPropertiesPanel = () => {
 				transformInput("H", "transformHeight", "height"),
 				transformInput("W", "transformWidth", "width"),
 			],
+			[
+				{
+					label: "Height",
+					name: "isDynamicHeight",
+					isLabelled: true,
+					labelDirection: "column",
+					condtional: () => MainStore.getCurrentElementsValues[0],
+					frappeControl: (ref, name) => {
+						const MainStore = useMainStore();
+						makeFeild({
+							name,
+							ref,
+							fieldtype: "Select",
+							requiredData: [MainStore.getCurrentElementsValues[0]],
+							reactiveObject: () => MainStore.getCurrentElementsValues[0],
+							propertyName: "isDynamicHeight",
+							isStyle: false,
+							options: () => [
+								{ label: "Auto", value: "auto" },
+								{ label: "Fixed", value: "fixed" },
+							],
+							formatValue: (object, property, isStyle) => {
+								if (!object) return;
+								return object[property] ? "auto" : "fixed";
+							},
+							onChangeCallback: (value = null) => {
+								if (value && MainStore.getCurrentElementsValues[0]) {
+									MainStore.getCurrentElementsValues[0]["isDynamicHeight"] =
+										value === "auto";
+									MainStore.frappeControls[name].$input.blur();
+								}
+							},
+						});
+					},
+					flex: 1,
+				},
+			],
 		],
 	});
 	MainStore.propertiesPanel.push({
@@ -663,7 +700,10 @@ export const createPropertiesPanel = () => {
 										}
 									}
 								}
-								if (MainStore.getCurrentElementsValues[0]?.selectedColumn && value != "main"){
+								if (
+									MainStore.getCurrentElementsValues[0]?.selectedColumn &&
+									value != "main"
+								) {
 									MainStore.getCurrentElementsValues[0].selectedColumn = null;
 								}
 							},
