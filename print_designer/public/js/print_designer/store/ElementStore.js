f79baba786246ba03f8219022d0245f91252d4b7
Maharshi Patel <maharshi.patel988@gmail.com>
2024-03-27 19:49:03 +0530

    refactor!: logic for saving elements.
    -   computeLayout function loops over this.Elements
        divides them into possible rows ( dynamic containers ) based on their start and end locations.
        returns three arrays, header, body ( can have multiple containers ) and footer.
    
    -   separated logic into smaller functions and changed structure for save a bit for dynamic containers.
        -   checkIfAnyTableIsEmpty
        -   handleHeaderFooterOverlapping
            -   autoCalculateHeaderFooter
        -   cleanUpElementsForSave
            -   checkIfPrintFormatIsEmpty
            -   childrensSave

--- print_designer/public/js/print_designer/store/ElementStore.js
+++ print_designer/public/js/print_designer/store/ElementStore.js
@@ -8,7 +8,7 @@ import {
 	createTable,
 	createBarcode,
 } from "../defaultObjects";
-import { handlePrintFonts } from "../utils";
+import { handlePrintFonts, setCurrentElement } from "../utils";
 export const useElementStore = defineStore("ElementStore", {
 	state: () => ({
 		Elements: new Array(),
@@ -34,260 +34,30 @@ export const useElementStore = defineStore("ElementStore", {
 			}
 			return newElement;
 		},
-		childrensSave(element, printFonts) {
-			let saveEl = { ...element };
-			delete saveEl.DOMRef;
-			delete saveEl.index;
-			delete saveEl.snapPoints;
-			delete saveEl.snapEdges;
-			delete saveEl.parent;
-			if (printFonts && ["text", "table"].indexOf(saveEl.type) != -1) {
-				handlePrintFonts(saveEl, printFonts);
-			}
-			if (saveEl.type == "rectangle") {
-				const childrensArray = saveEl.childrens;
-				saveEl.childrens = [];
-				childrensArray.forEach((el) => {
-					const child = this.childrensSave(el, printFonts);
-					child && saveEl.childrens.push(child);
-				});
-			}
-
-			return saveEl;
-		},
 		async saveElements() {
 			const MainStore = useMainStore();
-			if (MainStore.mode == "preview") return;
-			let mainPrintFonts = {};
-			let headerPrintFonts = {};
-			let footerprintFonts = {};
-			const headerElements = [];
-			const mainElements = [];
-			const afterTableElements = [];
-			const footerElements = [];
-			let tableElement = this.Elements.filter((el) => el.type == "table");
-			if (tableElement.some((el) => el.table == null)) {
-				let message = __("You have Empty Table. Please add table fields or remove table.");
-				frappe.show_alert(
-					{
-						message: message,
-						indicator: "red",
-					},
-					5
-				);
-				return;
-			}
-			let isOverlapping = false;
-			if (tableElement.length == 1 && MainStore.isHeaderFooterAuto) {
-				this.Elements.forEach((element) => {
-					if (
-						(element.startY < tableElement[0].startY + MainStore.page.marginTop &&
-							element.startY + element.height >
-								tableElement[0].startY + MainStore.page.marginTop) ||
-						(element.startY <
-							MainStore.page.height -
-								(MainStore.page.height -
-									(tableElement[0].startY + tableElement[0].height)) -
-								MainStore.page.marginTop -
-								MainStore.page.marginBottom &&
-							element.startY + element.height >
-								MainStore.page.height -
-									(MainStore.page.height -
-										(tableElement[0].startY + tableElement[0].height)) -
-									MainStore.page.marginTop -
-									MainStore.page.marginBottom)
-					) {
-						isOverlapping = true;
-					}
-				});
-				if (isOverlapping) {
-					MainStore.mode = "editing";
-				} else if (!isOverlapping) {
-					MainStore.page.headerHeight = tableElement[0].startY;
-					MainStore.page.footerHeight =
-						MainStore.page.height -
-						(tableElement[0].startY +
-							tableElement[0].height +
-							MainStore.page.marginTop +
-							MainStore.page.marginBottom);
-				}
-			} else {
-				let isHeaderOverlapping = false;
-				let isFooterOverlapping = false;
-				this.Elements.forEach((element) => {
-					if (
-						element.startY < MainStore.page.headerHeight &&
-						element.startY + element.height > MainStore.page.headerHeight
-					) {
-						isHeaderOverlapping = true;
-					}
-					if (
-						element.startY <
-							MainStore.page.height -
-								MainStore.page.footerHeight -
-								MainStore.page.marginTop -
-								MainStore.page.marginBottom &&
-						element.startY + element.height >
-							MainStore.page.height -
-								MainStore.page.footerHeight -
-								MainStore.page.marginTop -
-								MainStore.page.marginBottom
-					) {
-						isFooterOverlapping = true;
-					}
-				});
-				if (isHeaderOverlapping || isFooterOverlapping) {
-					MainStore.mode = "pdfSetup";
-					frappe.show_alert(
-						{
-							message: `Please resolve overlapping ${
-								isHeaderOverlapping ? "header" : ""
-							} ${isHeaderOverlapping && isFooterOverlapping ? " and " : ""} ${
-								isFooterOverlapping ? "footer" : ""
-							}`,
-							indicator: "red",
-						},
-						5
-					);
-					return;
-				}
-			}
-			let isHeaderEmpty = true;
-			let isBodyEmpty = true;
-			let isFooterEmpty = true;
-			let pageInfoInBody = [];
-			if (tableElement.length == 1) {
-				tableElement[0].isPrimaryTable = true;
-			} else if (tableElement.length > 1) {
-				let primaryTableEl = tableElement.filter((el) => el.isPrimaryTable);
-				if (primaryTableEl.length == 1) {
-					tableElement = primaryTableEl;
-				} else {
-					const message = __(
-						"As You have multiple tables, you have to select Primary Table. <br></br> 1. Go to Table Element that you wish to set as Primary. <br></br> 2. Select it and from properties panel select <b>Set as Primary Table</b> as <b>Yes</b> "
-					);
-					frappe.msgprint(
-						{
-							title: __("Multiple Tables."),
-							message: message,
-							indicator: "red",
-						},
-						5
-					);
-					return;
-				}
-			}
-			this.Elements.forEach((element) => {
-				let is_header = false;
-				let is_footer = false;
-				if (
-					element.startY + element.height <
-					MainStore.page.headerHeight + MainStore.page.marginTop
-				) {
-					is_header = true;
-				} else if (
-					element.startY >
-					MainStore.page.height -
-						MainStore.page.footerHeight -
-						MainStore.page.marginTop -
-						MainStore.page.marginBottom
-				) {
-					is_footer = true;
-				} else {
-					if (element.type == "text" && element.isDynamic) {
-						element.dynamicContent
-							.filter(
-								(el) =>
-									["page", "topage", "date", "time"].indexOf(el.fieldname) != -1
-							)
-							.forEach((field) => {
-								pageInfoInBody.push(field.fieldname);
-							});
-					}
-				}
-				let printFonts = is_header
-					? headerPrintFonts
-					: is_footer
-					? footerprintFonts
-					: mainPrintFonts;
-				let newElement = this.childrensSave(element, printFonts);
-				newElement.classes = newElement.classes.filter(
-					(name) => ["inHeaderFooter", "overlappingHeaderFooter"].indexOf(name) == -1
-				);
-				if (element.type == "rectangle" && element.childrens.length) {
-					let childrensArray = element.childrens;
-					newElement.childrens = [];
-					childrensArray.forEach((el) => {
-						newElement.childrens.push(this.childrensSave(el, printFonts));
-					});
-				}
-				if (is_header) {
-					newElement.printY = newElement.startY + MainStore.page.marginTop;
-					MainStore.printHeaderFonts = printFonts;
-					headerElements.push(newElement);
-					isHeaderEmpty = false;
-				} else if (is_footer) {
-					newElement.printY =
-						newElement.startY -
-						(MainStore.page.height -
-							MainStore.page.footerHeight -
-							MainStore.page.marginBottom -
-							MainStore.page.marginTop);
-					MainStore.printFooterFonts = printFonts;
-					footerElements.push(newElement);
-					isFooterEmpty = false;
-				} else {
-					newElement.printY = newElement.startY - MainStore.page.headerHeight;
-					MainStore.printBodyFonts = printFonts;
-					if (
-						tableElement.length == 1 &&
-						tableElement[0].startY + tableElement[0].height <= newElement.startY + 2
-					) {
-						newElement.printY =
-							newElement.startY - (tableElement[0].startY + tableElement[0].height);
-						newElement.printX = newElement.startX - tableElement[0].startX;
+			if (this.checkIfAnyTableIsEmpty()) return;
+			const [headerElements, bodyElements, footerElements] = await this.computeLayout();
+			if (!this.handleHeaderFooterOverlapping(headerElements.flat())) return;
+			if (!this.handleHeaderFooterOverlapping(bodyElements.flat())) return;
+			if (!this.handleHeaderFooterOverlapping(footerElements.flat())) return;
 
-						afterTableElements.push(newElement);
-					} else {
-						mainElements.push(newElement);
-					}
-					isBodyEmpty = false;
-				}
-			});
-			if (isHeaderEmpty) {
-				MainStore.printHeaderFonts = null;
-			}
-			if (isBodyEmpty) {
-				if (!isHeaderEmpty || !isFooterEmpty) {
-					MainStore.mode = "pdfSetup";
-					frappe.show_alert(
-						{
-							message: "Atleast 1 element is required inside body",
-							indicator: "red",
-						},
-						5
-					);
-					return;
-				}
-				MainStore.printBodyFonts = null;
-			}
-			if (pageInfoInBody.length) {
-				frappe.show_alert({
-					message:
-						"Please move <b>" + pageInfoInBody.join(", ") + "</b> to header / footer",
-					indicator: "orange",
-				});
-				return;
-			}
-			if (isFooterEmpty) {
-				MainStore.printFooterFonts = null;
-			}
+			const header = this.cleanUpElementsForSave(headerElements, "header");
+			const body = this.cleanUpElementsForSave(bodyElements, "body");
+			const footer = this.cleanUpElementsForSave(footerElements, "footer");
+
+			if (!body) return;
+
+			const [cleanedBodyElements, bodyFonts] = body;
+			const [cleanedHeaderElements, headerFonts] = header || [[], null];
+			const [cleanedFooterElements, footerFonts] = footer || [[], null];
+
 			MainStore.currentFonts.length = 0;
 			MainStore.currentFonts.push(
 				...Object.keys({
-					...(headerPrintFonts || {}),
-					...(mainPrintFonts || {}),
-					...(footerprintFonts || {}),
+					...(headerFonts || {}),
+					...(bodyFonts || {}),
+					...(footerFonts || {}),
 				})
 			);
 			const updatedPage = { ...MainStore.page };
@@ -320,14 +90,17 @@ export const useElementStore = defineStore("ElementStore", {
 			const css =
 				convertCsstoString(MainStore.screenStyleSheet) +
 				convertCsstoString(MainStore.printStyleSheet);
-			await frappe.db.set_value("Print Format", MainStore.printDesignName, {
-				print_designer_header: JSON.stringify(headerElements),
-				print_designer_body: JSON.stringify(mainElements),
-				print_designer_after_table: JSON.stringify(afterTableElements),
-				print_designer_footer: JSON.stringify(footerElements),
+
+			const objectToSave = {
+				print_designer_header: JSON.stringify(cleanedHeaderElements[0]),
+				print_designer_body: JSON.stringify(cleanedBodyElements.flat()),
+				print_designer_after_table: null,
+				print_designer_footer: JSON.stringify(cleanedFooterElements[0]),
 				print_designer_settings: JSON.stringify(settingsForSave),
-				css,
-			});
+				css: css,
+			};
+
+			await frappe.db.set_value("Print Format", MainStore.printDesignName, objectToSave);
 			await frappe.dom.unfreeze();
 			frappe.show_alert(
 				{
@@ -337,6 +110,194 @@ export const useElementStore = defineStore("ElementStore", {
 				5
 			);
 		},
+		checkIfAnyTableIsEmpty() {
+			const emptyTable = this.Elements.find((el) => el.type == "table" && el.table == null);
+			if (emptyTable) {
+				let message = __("You have Empty Table. Please add table fields or remove table.");
+				setCurrentElement({}, emptyTable);
+				frappe.show_alert(
+					{
+						message: message,
+						indicator: "red",
+					},
+					5
+				);
+				return true;
+			}
+			return false;
+		},
+		async computeLayout(element = null) {
+			const MainStore = useMainStore();
+			const elements = [...this.Elements].map((el, index) => {
+				return {
+					index,
+					startY: parseInt(el.startY),
+					endY: parseInt(el.startY + el.height),
+					element: el,
+				};
+			});
+			elements.sort((a, b) => {
+				return a.startY < b.startY ? -1 : 1;
+			});
+			const fullWidthElements = elements.filter(
+				(currentEl) => !currentEl.element.isElementOverlapping
+			);
+			const headerContainer = [];
+			const bodyContainer = [];
+			const footerContainer = [];
+			const tempElementsArray = [];
+			elements.forEach((currentEl) => {
+				if (MainStore.page.headerHeight && currentEl.endY <= MainStore.page.headerHeight) {
+					headerContainer.push(currentEl);
+				} else if (
+					MainStore.page.footerHeight &&
+					currentEl.startY >= MainStore.page.height - MainStore.page.footerHeight
+				) {
+					footerContainer.push(currentEl);
+				} else if (
+					fullWidthElements.includes(currentEl) &&
+					currentEl.element.isDynamicHeight
+				) {
+					if (tempElementsArray.length) {
+						bodyContainer.push([...tempElementsArray]);
+					}
+					bodyContainer.push([currentEl]);
+					tempElementsArray.length = 0;
+				} else {
+					tempElementsArray.push(currentEl);
+				}
+			});
+			if (tempElementsArray.length) {
+				bodyContainer.push(tempElementsArray);
+			}
+			return [[headerContainer], bodyContainer, [footerContainer]];
+		},
+		handleHeaderFooterOverlapping(elements) {
+			const MainStore = useMainStore();
+			const tableElement = this.Elements.filter((el) => el.type == "table");
+			let isOverlapping = false;
+
+			if (tableElement.length == 1 && MainStore.isHeaderFooterAuto) {
+				isOverlapping = !this.autoCalculateHeaderFooter(tableElement[0]);
+			} else {
+				isOverlapping = elements.some((element) => {
+					element = element.element;
+					if (
+						(element.startY < MainStore.page.headerHeight &&
+							element.startY + element.height > MainStore.page.headerHeight) ||
+						(element.startY <
+							MainStore.page.height -
+								MainStore.page.footerHeight -
+								MainStore.page.marginTop -
+								MainStore.page.marginBottom &&
+							element.startY + element.height >
+								MainStore.page.height -
+									MainStore.page.footerHeight -
+									MainStore.page.marginTop -
+									MainStore.page.marginBottom)
+					) {
+						return true;
+					}
+					return false;
+				});
+			}
+			if (!isOverlapping) return true;
+			MainStore.mode = "pdfSetup";
+			frappe.show_alert(
+				{
+					message: "Please resolve overlapping header/footer elements",
+					indicator: "red",
+				},
+				5
+			);
+		},
+		autoCalculateHeaderFooter(tableEl) {
+			const MainStore = useMainStore();
+
+			if (this.isElementOverlapping(tableEl)) return false;
+
+			MainStore.page.headerHeight = tableEl.startY;
+			MainStore.page.footerHeight =
+				MainStore.page.height -
+				(tableEl.startY +
+					tableEl.height +
+					MainStore.page.marginTop +
+					MainStore.page.marginBottom);
+
+			return true;
+		},
+		cleanUpElementsForSave(elements, type) {
+			if (this.checkIfPrintFormatIsEmpty(elements, type)) return;
+			const fontsArray = [];
+			const cleanedElements = [];
+			elements.forEach((container) => {
+				const cleanedContainer = [];
+				container.forEach((element) => {
+					let newElement = this.childrensSave(element.element, fontsArray);
+					newElement.classes = newElement.classes.filter(
+						(name) => ["inHeaderFooter", "overlappingHeaderFooter"].indexOf(name) == -1
+					);
+					if (element.type == "rectangle" && element.childrens.length) {
+						let childrensArray = element.childrens;
+						newElement.childrens = [];
+						childrensArray.forEach((el) => {
+							newElement.childrens.push(this.childrensSave(el, printFonts));
+						});
+					}
+					cleanedContainer.push(newElement);
+				});
+				cleanedElements.push(cleanedContainer);
+			});
+			return [cleanedElements, fontsArray];
+		},
+		checkIfPrintFormatIsEmpty(elements, type) {
+			const MainStore = useMainStore();
+			if (elements.length == 0) {
+				switch (type) {
+					case "header":
+						MainStore.printHeaderFonts = null;
+						break;
+					case "body":
+						MainStore.printBodyFonts = null;
+						frappe.show_alert(
+							{
+								message: "Atleast 1 element is required inside body",
+								indicator: "red",
+							},
+							5
+						);
+						// This is intentionally using throw to stop the execution
+						throw new Error(__("Atleast 1 element is required inside body"));
+					case "footer":
+						MainStore.printFooterFonts = null;
+						break;
+				}
+				return true;
+			}
+			return false;
+		},
+		childrensSave(element, printFonts) {
+			let saveEl = { ...element };
+			delete saveEl.DOMRef;
+			delete saveEl.index;
+			delete saveEl.snapPoints;
+			delete saveEl.snapEdges;
+			delete saveEl.parent;
+			if (printFonts && ["text", "table"].indexOf(saveEl.type) != -1) {
+				handlePrintFonts(saveEl, printFonts);
+			}
+			if (saveEl.type == "rectangle") {
+				const childrensArray = saveEl.childrens;
+				saveEl.childrens = [];
+				childrensArray.forEach((el) => {
+					const child = this.childrensSave(el, printFonts);
+					child && saveEl.childrens.push(child);
+				});
+			}
+
+			return saveEl;
+		},
+
 		handleDynamicContent(element) {
 			const MainStore = useMainStore();
 			if (
