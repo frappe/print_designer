bdbbd2c52383d22811dbe3a057115f4149fff9e2
Maharshi Patel <maharshi.patel988@gmail.com>
2024-04-01 13:33:10 +0530

    fix: export import preview images
    when print_designer format is saved in developer mode and format is standard.
    preview images and " File " document is exported as per the location defined in the selected app's hook.
    after install once the formats are imported, copy the file to private/files folder and update the file path in the format.

--- print_designer/default_formats.py
+++ print_designer/default_formats.py
@@ -1,8 +1,10 @@
 import os
+import shutil
 from pathlib import Path
 
 import frappe
 from frappe.modules.import_file import import_file_by_path
+from frappe.utils import get_files_path
 
 """
 features:
@@ -27,6 +29,27 @@ def on_print_designer_install():
 		install_default_formats(app=app, load_pd_formats=False)
 
 
+def get_preview_image_folder_path(print_format):
+	app = frappe.scrub(frappe.get_doctype_app(print_format.doc_type))
+	pd_folder = frappe.get_hooks(
+		"pd_standard_format_folder", app_name=print_format.print_designer_template_app
+	)
+	if len(pd_folder) == 0:
+		pd_folder = ["default_templates"]
+	return os.path.join(
+		frappe.get_app_path(print_format.print_designer_template_app), os.path.join(pd_folder[0], app)
+	)
+
+
+def update_preview_img(file):
+	print_format = frappe.get_doc(file.attached_to_doctype, file.attached_to_name)
+	folder = get_preview_image_folder_path(print_format)
+	file_name = print_format.print_designer_preview_img.split("/")[-1]
+	org_path = os.path.join(folder, file_name)
+	target_path = get_files_path(file_name, is_private=1)
+	shutil.copy2(org_path, target_path)
+
+
 # called after install of any new app.
 def install_default_formats(app, filter_by="", load_pd_formats=True):
 	if load_pd_formats:
@@ -42,9 +65,29 @@ def install_default_formats(app, filter_by="", load_pd_formats=True):
 		app=app, templates_folder=pd_folder[0], filter_by=filter_by
 	)
 
+	preview_files = [f for f in print_formats if f.name.endswith("-preview.json")]
+	print_formats = [f for f in print_formats if not f.name.endswith("-preview.json")]
+
 	for json_file_path in print_formats:
-		import_file_by_path(json_file_path)
+		import_file_by_path(path=json_file_path)
 		frappe.db.commit()
+
+	for json_file_path in preview_files:
+		import_file_by_path(path=json_file_path, pre_process=update_preview_img)
+		frappe.db.commit()
+
+	for pf in frappe.db.get_all("Print Format", filters={"standard": "Yes", "print_designer": 1}):
+		updated_url = frappe.db.get_value(
+			"File",
+			{
+				"attached_to_doctype": "Print Format",
+				"attached_to_name": pf.name,
+				"attached_to_field": "print_designer_preview_img",
+			},
+			"file_url",
+		)
+		if updated_url:
+			frappe.set_value("Print Format", pf.name, "print_designer_preview_img", updated_url)
 
 
 def get_filtered_formats_by_app(app, templates_folder, filter_by=""):
--- print_designer/print_designer/overrides/print_format.py
+++ print_designer/print_designer/overrides/print_format.py
@@ -9,7 +9,7 @@ from frappe.printing.doctype.print_format.print_format import PrintFormat
 class PDPrintFormat(PrintFormat):
 	def export_doc(self):
 		if (
-			not self.standard
+			not self.standard == "Yes"
 			or not frappe.conf.developer_mode
 			or frappe.flags.in_patch
 			or frappe.flags.in_install
@@ -36,8 +36,8 @@ class PDPrintFormat(PrintFormat):
 
 		# write the data file
 		path = os.path.join(folder, f"{fname}.json")
-		with open(path, "w+") as jsonfile:
-			jsonfile.write(frappe.as_json(doc_export))
+		with open(path, "w+") as json_file:
+			json_file.write(frappe.as_json(doc_export))
 		print(f"Wrote document file for {doc.doctype} {doc.name} at {path}")
 		self.export_preview(folder=folder, fname=fname)
 
@@ -57,8 +57,28 @@ class PDPrintFormat(PrintFormat):
 
 	def export_preview(self, folder, fname):
 		if self.print_designer_preview_img:
-			file = frappe.get_doc("File", {"file_url": self.print_designer_preview_img})
+			try:
+				file = frappe.get_doc(
+					"File",
+					{
+						"file_url": self.print_designer_preview_img,
+						"attached_to_doctype": self.doctype,
+						"attached_to_name": self.name,
+						"attached_to_field": "print_designer_preview_img",
+					},
+				)
+			except frappe.DoesNotExistError:
+				file = None
+			if not file:
+				return
+			file_export = file.as_dict(no_nulls=True)
+			file.run_method("before_export", file_export)
 			org_path = file.get_full_path()
-			target_path = os.path.join(folder, f"{fname}-preview.jpg")
+			target_path = os.path.join(folder, org_path.split("/")[-1])
 			shutil.copy2(org_path, target_path)
 			print(f"Wrote preview file for {self.doctype} {self.name} at {target_path}")
+			# write the data file
+			path = os.path.join(folder, f"print_designer-{fname}-preview.json")
+			with open(path, "w+") as json_file:
+				json_file.write(frappe.as_json(file_export))
+			print(f"Wrote document file for {file.doctype} {file.name} at {path}")
