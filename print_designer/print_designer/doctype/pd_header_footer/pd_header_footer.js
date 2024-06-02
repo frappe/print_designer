1d5caf92f35aaddf8a1ece840ec10a7eb16c7f02
Maharshi Patel <maharshi.patel988@gmail.com>
2024-06-02 13:43:54 +0530

    chore: better structure for header/footer doctype

--- print_designer/print_designer/doctype/pd_header_footer/pd_header_footer.js
+++ print_designer/print_designer/doctype/pd_header_footer/pd_header_footer.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2024, Frappe Technologies Pvt Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("PD Header Footer", {
-// 	refresh(frm) {
+const set_template_app_options = (frm) => {
+	frappe.xcall("frappe.core.doctype.module_def.module_def.get_installed_apps").then((r) => {
+		frm.set_df_property("export_template_app", "options", JSON.parse(r));
+		if (!frm.doc.export_template_app) {
+			frm.set_value("export_template_app", "print_designer");
+		}
+	});
+};
 
-// 	},
-// });
+frappe.ui.form.on("PD Header Footer", {
+	refresh(frm) {
+		set_template_app_options(frm);
+	},
+});
