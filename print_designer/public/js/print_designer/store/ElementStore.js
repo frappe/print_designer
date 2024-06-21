f70ed96f7663b037e0cd0076393eb574283b9108
IamSaiyyamChhetri <IamSaiyyamChhetri>
2024-06-21 14:48:12 +0530

    fix : solve conflict issue

--- print_designer/public/js/print_designer/store/ElementStore.js
+++ print_designer/public/js/print_designer/store/ElementStore.js
@@ -503,18 +503,6 @@ export const useElementStore = defineStore("ElementStore", {
 					});
 					throw new Error(message);
 			}
-<<<<<<< HEAD
-			if (MainStore.frappeControls['rawCmdLang'].value == null){
-				frappe.show_alert(
-					{
-						message: "Please select the language for raw cmd",
-						indicator: "red",
-				});
-				throw new Error('Please select the language for raw cmd');
-			}
-
-=======
->>>>>>> 8afaa37 (feat : added validation for field and overlapping error msg)
 			const tableElement = this.Elements.filter((el) => el.type == "table");
 			if(this.isParentElementOverlapping(elements)){
 				throwOverlappingError("element", false);
