873e7cd8c1dad6f4eebe80f64df3b005141b1621
Packeting <packeting.zhong@yoswit.com>
2026-02-05 14:13:47 +0800

    fix(commands): init site context before setup-chrome and fix chrome typo

--- print_designer/commands/__init__.py
+++ print_designer/commands/__init__.py
@@ -1,11 +1,26 @@
 import click
+import frappe
+from frappe.commands import pass_context
+from frappe.exceptions import SiteNotSpecifiedError
+from frappe.utils.bench_helper import CliCtxObj
 
 
 @click.command("setup-chrome", help="setup chrome (server-side) for pdf generation")
-def setup_chorme():
+@pass_context
+def setup_chrome(context: CliCtxObj):
 	from print_designer.install import setup_chromium
 
-	setup_chromium()
+	if not context.sites:
+		raise SiteNotSpecifiedError
+
+	for site in context.sites:
+		try:
+			frappe.init(site)
+			frappe.connect()
+			setup_chromium()
+			frappe.db.commit()
+		finally:
+			frappe.destroy()
 
 
-commands = [setup_chorme]
+commands = [setup_chrome]
